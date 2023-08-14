be8b50cad51a725af47749a0e5c4ac9e855e3d0f
ibrahimfqaisi <qasrawi.ibrahim1998@gmail.com>
2023-08-14 18:28:48 +0000

    i solved lab37

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,103 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import { useState } from "react";
 
 export default function Home() {
+  // State to store the cookie stand data
+  const [cookieStands, setCookieStands] = useState([]);
+
+  // Function to add a new cookie stand
+  const addCookieStand = (event) => {
+    event.preventDefault();
+
+    // Create a new cookie stand object
+    const newCookieStand = {
+      id: Math.random().toString(36).substring(7), // Generate a unique ID
+      location: event.target.location.value,
+      minCustomersPerHour: parseInt(event.target.minCustomersPerHour.value),
+      maxCustomersPerHour: parseInt(event.target.maxCustomersPerHour.value),
+      avgCookiesPerSale: parseFloat(event.target.avgCookiesPerSale.value),
+    };
+
+    // Update the cookie stands state with the new cookie stand
+    setCookieStands([...cookieStands, newCookieStand]);
+
+    // Reset the form fields
+    event.target.reset();
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div>
+      <Head>
+        <title>Cookie Stand Admin</title>
+      </Head>
+      <body className="flex flex-col min-h-screen">
+        <header className="p-4 text-white bg-green-500">
+          <h1 className="text-4xl">Cookie Stand Admin</h1>
+        </header>
+        <main className="flex flex-col items-center flex-grow py-4 space-y-8">
+          <h2 className="text-2xl">Create Cookie Stand</h2>
+          <form
+            onSubmit={addCookieStand}
+            className="flex flex-col p-2 space-y-2 bg-gray-100"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <label>
+              Location:
+              <input
+                type="text"
+                name="location"
+                className="p-1 border rounded"
+                required
+              />
+            </label>
+            <label>
+              Min Customers per Hour:
+              <input
+                type="number"
+                name="minCustomersPerHour"
+                className="p-1 border rounded"
+                required
+              />
+            </label>
+            <label>
+              Max Customers per Hour:
+              <input
+                type="number"
+                name="maxCustomersPerHour"
+                className="p-1 border rounded"
+                required
+              />
+            </label>
+            <label>
+              Avg Cookies per Sale:
+              <input
+                type="number"
+                step="0.1"
+                name="avgCookiesPerSale"
+                className="p-1 border rounded"
+                required
+              />
+            </label>
+            <button
+              type="submit"
+              className="p-2 text-white bg-green-500 rounded"
+            >
+              Create
+            </button>
+          </form>
+          {cookieStands.length === 0 ? (
+            <p className="mt-4 text-center text-gray-600">
+              Report Table Coming Soon...
+            </p>
+          ) : (
+            <pre className="p-2 mt-4 bg-gray-200">
+              {JSON.stringify(cookieStands, null, 2)}
+            </pre>
+          )}
+        </main>
+        <footer className="p-4 text-center text-white bg-gray-500">
+          &copy; ASAC 2023
+        </footer>
+      </body>
+    </div>
+  );
 }
